60e3b5da8b3bac646ddd010bf1ecc7a8d0567556
casangwe <casangwe@hotmail.com>
2024-11-15 18:10:02 -0600

    Add production build of frontend

---
+++ trading-frontend/build/precache-manifest.3ed46b37e9182110948b4c3cc5a4939e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7098d284de68feaf332dab81e305ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eae393f4176bf1ba3b4d",
+    "url": "/static/css/main.6e435bc7.chunk.css"
+  },
+  {
+    "revision": "22ee278918898862bf81",
+    "url": "/static/js/2.ce99a6d3.chunk.js"
+  },
+  {
+    "revision": "26d0496e307c2bc406616a4d336b512c",
+    "url": "/static/js/2.ce99a6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae393f4176bf1ba3b4d",
+    "url": "/static/js/main.89f69e80.chunk.js"
+  },
+  {
+    "revision": "8c5cb845df46c26bab4a",
+    "url": "/static/js/runtime-main.eebc18e9.js"
+  }
+]);
